e44ccff3e3e8803157ca0ccbd7a43c45ce4df4c2
chewie <chewie.louveau@gmail.com>
2025-07-24 22:23:50 +0200

    Change themes

---
+++ lua/plugins/color.lua
@@ -0,0 +1,17 @@
+return {
+  "rose-pine/nvim",
+  name = "rose-pine",
+  priority = 1000,
+  config = function()
+    require("rose-pine").setup({
+      variant = "moon",
+      dark_variant = "moon",
+      styles = {
+        italic = false,
+        bold = true,
+        transparent = false,
+      },
+    })
+    vim.cmd.colorscheme("rose-pine")
+  end,
+}
