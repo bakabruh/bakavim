aa6c746798221cf8c30e1fc14cabe845beea4fed
chewie <chewie.louveau@gmail.com>
2025-07-24 21:34:10 +0200

    ADD remaps

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -7,6 +7,9 @@ return {
 				null_ls.builtins.formatting.stylua,
 				null_ls.builtins.diagnostics.eslint_d,
 				null_ls.builtins.formatting.prettier,
+        null_ls.builtins.formatting.gofumpt,
+        null_ls.builtins.formatting.goimports,
+        null_ls.builtins.diagnostics.staticcheck,
 			},
 		})
 		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -6,5 +6,14 @@ vim.cmd("set number")
 vim.cmd("set relativenumber")
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", ":Ex<CR>", {})
-vim.keymap.set("n", "A-j", ":m .+1<CR>==", {})
-vim.keymap.set("n", "A-k", ":m .-2<CR>==", {})
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", {})
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", {})
+vim.keymap.set("n", "<C-d>", "<C-d>zz", {})
+vim.keymap.set("n", "<C-u>", "<C-u>zz", {})
+vim.keymap.set("n", "n", "nzzzv", {})
+vim.keymap.set("n", "N", "Nzzzv", {})
+vim.keymap.set("i", "<C-c>", "<Esc>", {})
+
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+
